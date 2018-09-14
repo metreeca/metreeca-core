6c2ee46321c344027883c307335915570ccfd892
Alessandro Bollini <alessandro.bollini@metreeca.com>
2018-09-14 19:41:07 +0200

    Recover Launcher wrapper

--- next/src/main/java/com/metreeca/next/wrappers/Launcher.java
+++ next/src/main/java/com/metreeca/next/wrappers/Launcher.java
@@ -15,55 +15,57 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.metreeca.rest.wrappers;
+package com.metreeca.next.wrappers;
 
-import com.metreeca.rest.*;
 import com.metreeca.form.things.Transputs;
+import com.metreeca.next.*;
+import com.metreeca.next.formats.Text;
 import com.metreeca.tray.sys.Loader;
 
 import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
 
-import static com.metreeca.tray._Tray.tool;
-
-import static java.lang.String.format;
+import static com.metreeca.tray.Tray.tool;
 
 
 /**
  * Single page app launcher (work in progress…).
  *
- * <p>Replaces {@linkplain Response.Reader#interactive() non-interactive responses} to {@linkplain
- * Request#interactive() interactive requests} with a relocated version of a (x)html page where absolute {@code href} or
- * {@code src} links are relocated to the page home folder.</p>
+ * <p>Replaces non-interactive responses to {@linkplain Message#interactive() interactive requests} with a relocated
+ * version of a (x)html page where absolute {@code href} or {@code src} links are relocated to the page home
+ * folder.</p>
+ *
+ * @deprecated Work in progress
  */
-public final class Launcher implements Wrapper {
+@Deprecated final class Launcher implements Wrapper {
 
 	private static final Pattern LinkPattern=Pattern.compile("\\b(href|src)\\s*=\\s*([\"'])/(.*)\\2");
 	private static final String LinkReplacement="$1=$2%s%s$3$2";
 
 
-	/**
-	 * Creates a new single page app launcher for the default page path.
-	 *
-	 * <p>Equivalent to {@link #launcher(String) launcher("/index.html")}.</p>
-	 *
-	 * @return the new single page app launcher
-	 */
-	public static Launcher launcher() {
-		return launcher("/index.html");
-	}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private String path=""; // the path of the app folder (no leading slash, trailing slash)
+	private String type=""; // the MIME type of the app (derived from page filename extension)
+	private String text=""; // the raw text of the app page
+
+	private final Loader loader=tool(Loader.Factory);
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	/**
-	 * Creates a new single page app launcher.
+	 * Configures the path of the app page.
 	 *
 	 * @param path the absolute path of the app page (to be loaded through the {@linkplain Loader#Factory system loader}
 	 *             tool)
 	 *
-	 * @return the new single page app launcher
+	 * @return this launcher
 	 *
+	 * @throws NullPointerException     if {@code path} is {@code null}
 	 * @throws IllegalArgumentException if {@code path} is not absolute
 	 */
-	public static Launcher launcher(final String path) {
+	public Launcher path(final String path) {
 
 		if ( path == null ) {
 			throw new NullPointerException("null path");
@@ -73,17 +75,6 @@ public final class Launcher implements Wrapper {
 			throw new IllegalArgumentException("relative path ["+path+"]");
 		}
 
-		return new Launcher(path);
-	}
-
-
-	private final String path; // the path of the app folder (no leading slash, trailing slash)
-	private final String type; // the MIME type of the app (derived from page filename extension)
-	private final String text; // the raw text of the app page
-
-
-	private Launcher(final String path) {
-
 		this.path=path // strip leading slash
 				.substring(1, path.lastIndexOf('/')+1);
 
@@ -91,40 +82,27 @@ public final class Launcher implements Wrapper {
 				: path.endsWith(".xhtml") ? "application/xhtml+xml"
 				: "";
 
-		this.text=tool(Loader.Factory)
+		this.text=loader
 				.load(path)
 				.map(Transputs::reader)
 				.map(Transputs::text)
 				.orElseThrow(() -> new NoSuchElementException("missing app page ["+path+"]"));
 
+		return this;
 	}
 
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	@Override public Handler wrap(final Handler handler) {
-		return (request, response) -> handler.handle(
-
-				writer ->
-
-						writer.copy(request).done(),
-
-				reader -> {
-
-					if ( request.interactive() && !reader.interactive() ) {
-
-						response.status(Response.OK)
-								.header("Content-Type", type)
-								.text(LinkPattern.matcher(text).replaceAll(
-										format(LinkReplacement, request.base(), path)
-								));
-
-					} else {
-
-						response.copy(reader).done();
-
-					}
-				}
-
-		);
+		return request -> handler.handle(request).map(response ->
+				path.isEmpty() || !request.interactive() || response.interactive() ? response : request
+						.response()
+						.status(Response.OK)
+						.header("Content-Type", type)
+						.body(Text.Format, LinkPattern.matcher(text).replaceAll(
+								String.format(LinkReplacement, request.base(), path)
+						)));
 	}
 
 }
