e4683aa30096bbbb4b3b42fae551733b3424a6c3
Alessandro Bollini <alessandro.bollini@metreeca.com>
2018-09-18 09:12:49 +0200

    Decouple gate module

---
+++ gate/src/main/java/com/metreeca/gate/handlers/Session.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright © 2013-2018 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca.
+ *
+ * Metreeca is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or(at your option) any later version.
+ *
+ * Metreeca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Metreeca.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.gate.handlers;
+
+import com.metreeca.gate.Roster;
+import com.metreeca.next.Handler;
+import com.metreeca.next.Request;
+import com.metreeca.next.Responder;
+import com.metreeca.next.handlers.Dispatcher;
+
+import org.eclipse.rdf4j.model.Value;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+
+import static com.metreeca.tray.Tray.tool;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+
+/**
+ * Basic session manager.
+ *
+ * <p>Manages session lifecycle interaction with the shared {@link Roster#Factory roster} tool.</p>
+ */
+final class Session implements Handler {
+
+	private static final String TicketMalformed="ticket-malformed";
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private final Roster roster=tool(Roster.Factory);
+
+	private final Dispatcher delegate=new Dispatcher().post(this::evolve);
+
+
+	@Override public Responder handle(final Request request) {
+		return delegate.handle(request);
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private Responder evolve(final Request request) {
+
+		throw new UnsupportedOperationException("to be implemented"); // !!! tbi
+
+		//try {
+		//
+		//	if ( !handle(request, response, request.json()) ) {
+		//		response.status(Response.BadRequest).body(JSON.Format, error(TicketMalformed));
+		//	}
+		//
+		//} catch ( final JsonException e ) {
+		//	response.status(Response.BadRequest).body(JSON.Format, error(TicketMalformed, e.getMessage()));
+		//}
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	//private boolean handle(final Request request, final Response response, final Object payload) {
+	//	return payload instanceof Map && handle(request, response, (Map<?, ?>)payload);
+	//}
+	//
+	//private boolean handle(final Request request, final Response response, final Map<?, ?> payload) {
+	//	return extend(request, response, payload)
+	//			|| change(request, response, payload)
+	//			|| update(request, response, payload);
+	//}
+	//
+	//private boolean extend(final Request request, final Response response, final Map<?, ?> payload) {
+	//	if ( validate(payload) ) {
+	//
+	//		final IRI user=request.user();
+	//
+	//		if ( user.equals(Form.none) ) {
+	//
+	//			response.status(Response.OK).body(JSON.Format, object());
+	//
+	//		} else {
+	//
+	//			final Roster.Permit permit=roster.refresh(user.stringValue());
+	//
+	//			if ( permit.valid(currentTimeMillis()) ) {
+	//
+	//				response.status(Response.OK).body(JSON.Format, ticket(permit));
+	//
+	//			} else {
+	//
+	//				response.status(Response.Forbidden).body(JSON.Format, Handler.error(permit.issue()));
+	//
+	//			}
+	//		}
+	//
+	//		return true;
+	//
+	//	} else {
+	//
+	//		return false;
+	//
+	//	}
+	//}
+	//
+	//private boolean change(final Request request, final Response response, final Map<?, ?> payload) {
+	//	if ( validate(payload, "usr", "pwd") ) {
+	//
+	//		final String usr=(String)payload.get("usr");
+	//		final String pwd=(String)payload.get("pwd");
+	//
+	//		final IRI current=request.user();
+	//
+	//		if ( usr.isEmpty() && pwd.isEmpty() ) { // logout
+	//
+	//			roster.release(current.stringValue());
+	//
+	//			response.status(Response.OK).body(JSON.Format, object());
+	//
+	//		} else { // switch user
+	//
+	//			final Roster.Permit permit=roster.acquire(usr, pwd);
+	//
+	//			if ( permit.valid(currentTimeMillis()) ) {
+	//
+	//				if ( !current.equals(permit.user()) ) {
+	//					roster.release(current.stringValue());
+	//				}
+	//
+	//				response.status(Response.OK).body(JSON.Format, ticket(permit));
+	//
+	//			} else {
+	//
+	//				response.status(Response.Forbidden).body(JSON.Format, Handler.error(permit.issue()));
+	//
+	//			}
+	//
+	//		}
+	//
+	//		return true;
+	//
+	//	} else {
+	//
+	//		return false;
+	//
+	//	}
+	//}
+	//
+	//private boolean update(final Request request, final Response response, final Map<?, ?> payload) {
+	//	if ( validate(payload, "usr", "old", "new") ) {
+	//
+	//		final String usr=(String)payload.get("usr");
+	//		final String old=(String)payload.get("old");
+	//		final String neu=(String)payload.get("new");
+	//
+	//		final IRI current=request.user();
+	//
+	//		// switch and activate user
+	//
+	//		final Roster.Permit permit=roster.acquire(usr, old, neu);
+	//
+	//		if ( permit.valid(currentTimeMillis()) ) {
+	//
+	//			if ( !current.equals(permit.user()) ) {
+	//				roster.release(current.stringValue());
+	//			}
+	//
+	//			response.status(Response.OK).body(JSON.Format, ticket(permit));
+	//
+	//		} else {
+	//
+	//			response.status(Response.Forbidden).body(JSON.Format, Handler.error(permit.issue()));
+	//
+	//		}
+	//
+	//		return true;
+	//
+	//	} else {
+	//
+	//		return false;
+	//
+	//	}
+	//}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private boolean validate(final JsonObject payload, final String... fields) {
+		return payload.keySet().equals(new HashSet<>(asList(fields)))
+				&& Arrays.stream(fields).allMatch(field -> payload.get(field) instanceof JsonString);
+	}
+
+	private JsonObject ticket(final Roster.Permit permit) { // !!! review user/roles reporting
+		return Json.createObjectBuilder()
+
+				.add("label", permit.label())
+				.add("token", permit.token())
+				.add("lease", permit.expiry())
+				.add("roles", Json.createArrayBuilder(permit
+						.roles()
+						.stream()
+						.map(Value::stringValue)
+						.collect(toList())
+				).build())
+
+				.build();
+	}
+
+}
---
+++ gate/src/main/java/com/metreeca/gate/rosters/_BasicRoster.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright © 2013-2018 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca.
+ *
+ * Metreeca is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or(at your option) any later version.
+ *
+ * Metreeca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Metreeca.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.gate.rosters;
+
+// Legacy key-based authentication (used by data and demo apps)
+// Salvage key handling and logout to KeyRoster
+
+
+public final class _BasicRoster {
+
+	//private static final String SysAdm="admin"; // !!! configurable?
+	//
+	//private static final Pattern BasicPattern=Pattern.compile("(?i:Basic\\s+(?<token>.*)\\s*)");
+	//
+	//
+	//private final String key;
+	//
+	//
+	//public _BasicRoster() {
+	//
+	//
+	//	final Setup setup=tool(Setup.Factory);
+	//
+	//	this.key=setup.get(Setup.KeyProperty, "");
+	//}
+	//
+	//
+	////@Override public void authorize(final Tool.Loader tools, final _Request request, final _Response response, final BiConsumer<_Request, _Response> sink) {
+	////
+	////	if ( response.getStatus() != 0 ) { sink.accept(request, response); } else {
+	////		final String authorization=request.getHeader("Authorization").orElse("");
+	////
+	////		final Matcher matcher=BasicPattern.matcher(authorization);
+	////
+	////		if ( matcher.matches() ) {
+	////
+	////			final String token=new String(Base64.getDecoder()
+	////					.decode(matcher.group("token")), Transputs.UTF8);
+	////
+	////			final int colon=max(token.indexOf(':'), 0);
+	////
+	////			final String usr=token.isEmpty() ? token : token.substring(0, colon);
+	////			final String pwd=token.isEmpty() ? token : token.substring(colon+1);
+	////
+	////			if ( usr.equals(SysAdm) && !key.isEmpty() && pwd.equals(key) ) {
+	////				request.setUser(iri(request.getBase())).setRoles(singleton(Form.root)); // !!! review usr name/iri
+	////			} else if ( !usr.equals("-") || !pwd.equals("-") ) { // ignore fake sign out credentials
+	////				response.setStatus(_Response.Unauthorized);
+	////			}
+	////
+	////		} else if ( !authorization.isEmpty() ) {
+	////			response.setStatus(_Response.Unauthorized).setText("unsupported authorization method");
+	////		}
+	////
+	////		sink.accept(request, response);
+	////	}
+	////}
+	//
+	////@Override public void authenticate(final Tool.Loader tools, final _Request request, final _Response response, final BiConsumer<_Request, _Response> sink) {
+	////
+	////	if ( response.getStatus() == _Response.Unauthorized ) {
+	////
+	////		sink.accept(request, response.addHeader("WWW-Authenticate", "Basic realm=\"metreeca\""));
+	////
+	////	} else {
+	////
+	////		sink.accept(request, response);
+	////
+	////	}
+	////}
+
+}
