b4b4deefd7fc3be2e6f6b22c8c2a395b3e2cc09c
Alessandro Bollini <alessandro.bollini@metreeca.com>
2018-10-07 10:34:00 +0200

    Recover Deleter

---
+++ rest/src/main/java/com/metreeca/rest/handlers/actors/Deleter.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2013-2018 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca.
+ *
+ *  Metreeca is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Affero General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or(at your option) any later version.
+ *
+ *  Metreeca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License along with Metreeca.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.rest.handlers.actors;
+
+
+import com.metreeca.form.Form;
+import com.metreeca.form.Shape;
+import com.metreeca.form.sparql.SPARQLEngine;
+import com.metreeca.rest.*;
+import com.metreeca.rest.handlers.Actor;
+import com.metreeca.tray.rdf.Graph;
+
+import org.eclipse.rdf4j.model.IRI;
+
+import static com.metreeca.form.Shape.empty;
+import static com.metreeca.tray.Tray.tool;
+
+
+public final class Deleter extends Actor<Deleter> {
+
+	private final Graph graph=tool(Graph.Factory);
+
+	public Deleter() {
+		delegate(handler(Form.delete, Form.detail, (request, shape) ->
+				empty(shape) ? direct(request) : driven(request, shape)
+		));
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private Responder direct(final Request request) {
+		return request.reply(response -> response.map(new Failure<>()
+				.status(Response.NotImplemented)
+				.cause("shapeless resource creation not supported"))
+		);
+	}
+
+	private Responder driven(final Request request, final Shape shape) {
+		return request.reply(response -> graph.update(connection -> {
+
+
+			final IRI focus=request.item();
+
+			if ( !connection.hasStatement(focus, null, null, true)
+					&& !connection.hasStatement(null, null, focus, true) ) {
+
+				// !!! 410 Gone if the resource is known to have existed (how to test?)
+
+				return response.status(Response.NotFound);
+
+			} else {
+
+				new SPARQLEngine(connection).delete(focus, shape);
+
+				return response.status(Response.NoContent);
+
+			}
+
+		}));
+	}
+
+}
---
+++ rest/src/test/java/com/metreeca/rest/handlers/actors/DeleterTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright © 2013-2018 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca.
+ *
+ *  Metreeca is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Affero General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or(at your option) any later version.
+ *
+ *  Metreeca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License along with Metreeca.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.rest.handlers.actors;
+
+
+import com.metreeca.rest.Request;
+import com.metreeca.rest.Response;
+import com.metreeca.tray.Tray;
+
+import org.eclipse.rdf4j.model.Model;
+import org.junit.jupiter.api.Test;
+
+import static com.metreeca.form.shapes.Or.or;
+import static com.metreeca.form.things.ValuesTest.*;
+import static com.metreeca.form.truths.ModelAssert.assertThat;
+import static com.metreeca.rest.HandlerAssert.graph;
+import static com.metreeca.rest.ResponseAssert.assertThat;
+import static com.metreeca.rest.formats.ShapeFormat.shape;
+
+
+final class DeleterTest {
+
+	private static final Model Dataset=small();
+
+
+	private void exec(final Runnable task) {
+		new Tray().exec(graph(small()), task).clear();
+	}
+
+
+	private Request direct() {
+		return new Request()
+				.roles(Manager)
+				.method(Request.DELETE)
+				.base(Base)
+				.path("/employees/1370");
+	}
+
+	private Request driven() {
+		return direct()
+				.body(shape()).set(Employee);
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Test void testDirectDelete() {
+		exec(() -> new Deleter()
+
+				.handle(driven())
+
+				.accept(response -> {
+
+					assertThat(response)
+							.hasStatus(Response.NoContent)
+							.doesNotHaveBody();
+
+					assertThat(graph("construct where { <employees/1370> ?p ?o }"))
+							.as("graph unchanged")
+							.isEmpty();
+
+				}));
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Test void testDrivenUnauthorized() {
+		exec(() -> new Deleter()
+
+				.handle(driven().roles(Salesman))
+
+				.accept(response -> {
+
+					assertThat(response)
+							.hasStatus(Response.Unauthorized)
+							.doesNotHaveBody();
+
+					assertThat(graph())
+							.as("graph unchanged")
+							.isIsomorphicTo(Dataset);
+
+				}));
+	}
+
+	@Test void testDrivenForbidden() {
+		exec(() -> new Deleter()
+
+				.handle(driven().body(shape()).set(or()))
+
+				.accept(response -> {
+
+					assertThat(response)
+							.hasStatus(Response.Forbidden)
+							.doesNotHaveBody();
+
+					assertThat(graph())
+							.as("graph unchanged")
+							.isIsomorphicTo(Dataset);
+
+				}));
+	}
+
+}
