95b5155cc1c360db756ffee534728a975144fe0a
Alessandro Bollini <alessandro.bollini@metreeca.com>
2019-09-23 11:08:15 +0200

    Rename gae module to gcloud

---
+++ back/gcloud/src/main/java/com/metreeca/gcloud/formats/EntityFormat.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright © 2013-2019 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca/Link.
+ *
+ * Metreeca/Link is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or(at your option) any later version.
+ *
+ * Metreeca/Link is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Metreeca/Link.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.gcloud.formats;
+
+import com.metreeca.gcloud.GCloud;
+import com.metreeca.gcloud.services.Datastore;
+import com.metreeca.rest.*;
+import com.metreeca.tree.Shape;
+import com.metreeca.tree.probes.Optimizer;
+import com.metreeca.tree.probes.Redactor;
+
+import com.google.cloud.datastore.BaseKey;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+
+import java.util.Optional;
+
+import javax.json.JsonValue;
+
+import static com.metreeca.gcloud.services.Datastore.datastore;
+import static com.metreeca.rest.Context.service;
+import static com.metreeca.rest.formats.JSONFormat.json;
+
+
+public final class EntityFormat implements Format<Entity> {
+
+	private static final EntityFormat Instance=new EntityFormat();
+
+
+	/**
+	 * Retrieves the entity body format.
+	 *
+	 * @return the singleton entity body format instance
+	 */
+	public static EntityFormat entity() {
+		return Instance;
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private final Datastore datastore=service(datastore());
+
+	private final EntityDecoder decoder=new EntityDecoder();
+	private final EntityEncoder encoder=new EntityEncoder();
+
+
+	private EntityFormat() {} // singleton
+
+
+	public Object value(final JsonValue value, final Shape shape) {
+		return decoder.decode(value, shape);
+	}
+
+
+	@Override public Result<Entity, Failure> get(final Message<?> message) {
+		return message.body(json()).value(json -> {
+
+			final FullEntity<?> entity=decoder.decode(json, shape(message));
+
+			return entity instanceof Entity ? (Entity)entity : Entity.newBuilder(
+
+					datastore.key(
+							Optional.ofNullable(entity.getKey()).map(BaseKey::getKind).orElse(GCloud.Resource),
+							message.request().path()
+					),
+
+					entity
+
+			).build();
+
+		});
+	}
+
+	@Override public <M extends Message<M>> M set(final M message, final Entity value) {
+		return message.body(json(),
+				encoder.encode(value, shape(message))
+		);
+	}
+
+
+	private Shape shape(final Message<?> message) {
+		return message.shape()
+
+				.map(new Redactor(Shape.Role))
+				.map(new Redactor(Shape.Task))
+				.map(new Redactor(Shape.Detail))
+				.map(new Redactor(Shape.Mode, Shape.Convey))
+
+				.map(new Optimizer());
+	}
+
+}
---
+++ back/gcloud/src/test/java/com/metreeca/gcloud/services/DatastoreDeleterTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright © 2013-2019 Metreeca srl. All rights reserved.
+ *
+ * This file is part of Metreeca/Link.
+ *
+ * Metreeca/Link is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or(at your option) any later version.
+ *
+ * Metreeca/Link is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Metreeca/Link.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.metreeca.gcloud.services;
+
+import com.metreeca.gcloud.GCloud;
+import com.metreeca.rest.Request;
+import com.metreeca.rest.Response;
+
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.EntityQuery;
+import com.google.cloud.datastore.Query;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.metreeca.gcloud.services.Datastore.datastore;
+import static com.metreeca.rest.Context.service;
+import static com.metreeca.rest.ResponseAssert.assertThat;
+import static com.metreeca.tree.shapes.Clazz.clazz;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+
+
+final class DatastoreDeleterTest extends DatastoreTestBase {
+
+	@Nested final class Container {
+
+		@Test void testReject() {
+			exec(() -> new DatastoreDeleter()
+
+					.handle(new Request()
+							.path("/entities/")
+					)
+
+					.accept(response -> assertThat(response)
+							.hasStatus(Response.InternalServerError)
+					)
+
+			);
+		}
+
+	}
+
+	@Nested final class Resource {
+
+		private List<Entity> entities(final String... codes) {
+			return Arrays.stream(codes).map(code -> Entity
+					.newBuilder(service(datastore()).key(GCloud.Resource, format("/entities/%s", code)))
+					.set("code", code)
+					.build()
+			).collect(toList());
+		}
+
+
+		@Test void testDelete() {
+			exec(load(() -> entities("1", "2", "3")), () -> new DatastoreDeleter()
+
+					.handle(new Request()
+							.path("/entities/1")
+							.shape(clazz(GCloud.Resource))
+					)
+
+					.accept(response -> {
+
+						assertThat(response)
+								.hasStatus(Response.NoContent)
+								.doesNotHaveBody();
+
+						service(datastore()).exec(service -> {
+
+							final EntityQuery query=Query.newEntityQueryBuilder()
+									.setKind(GCloud.Resource)
+									.build();
+
+							assertThat((Iterable<Entity>)() -> service.run(query))
+									.containsExactlyInAnyOrderElementsOf(entities("2", "3"));
+
+							return this;
+
+						});
+
+					})
+
+			);
+		}
+
+		@Test void testRejectMissing() {
+			exec(() -> new DatastoreDeleter()
+
+					.handle(new Request()
+							.path("/entities/9999")
+					)
+
+					.accept(response -> assertThat(response)
+							.hasStatus(Response.NotFound)
+							.doesNotHaveBody()
+					)
+
+			);
+		}
+
+	}
+
+}
