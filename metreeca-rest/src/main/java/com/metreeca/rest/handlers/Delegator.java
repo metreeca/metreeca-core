d79e70513f0655e29db48aaedd4b7ab28df6daaa
Alessandro Bollini <alessandro.bollini@metreeca.com>
2019-10-22 18:34:17 +0200

    Harden Delegator against self-delegation

--- metreeca-rest/src/main/java/com/metreeca/rest/handlers/Delegator.java
+++ metreeca-rest/src/main/java/com/metreeca/rest/handlers/Delegator.java
@@ -55,11 +55,16 @@ public abstract class Delegator implements Handler {
 	 * @return this delegator
 	 *
 	 * @throws NullPointerException if {@code delegate} is null
+	 * @throws IllegalArgumentException if {@code delegate} is equal to this handler
 	 */
 	protected Delegator delegate(final Handler delegate) {
 
 		if ( delegate == null ) {
 			throw new NullPointerException("null delegate");
+		}
+
+		if ( delegate.equals(this) ) {
+			throw new IllegalArgumentException("self delegate");
 		}
 
 		this.delegate=delegate;
