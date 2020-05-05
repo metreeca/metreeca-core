55dabc77e2c3fdfc4f46fa1aae72dba2529d1c54
Alessandro Bollini <22@metreeca.com>
2020-05-05 13:12:37 +0200

    Harden FileCache against corruption due to unhandled exceptions in encoders

--- metreeca-rest/src/main/java/com/metreeca/rest/services/Cache.java
+++ metreeca-rest/src/main/java/com/metreeca/rest/services/Cache.java
@@ -1,28 +1,12 @@
 /*
- * Copyright © 2013-2020 Metreeca srl. All rights reserved.
- *
- * This file is part of Metreeca/Link.
- *
- * Metreeca/Link is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Affero General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or(at your option) any later version.
- *
- * Metreeca/Link is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License along with Metreeca/Link.
- * If not, see <http://www.gnu.org/licenses/>.
+ * Copyright © 2020 Metreeca srl. All rights reserved.
  */
 
 package com.metreeca.rest.services;
 
 import com.metreeca.rest.Context;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.UncheckedIOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
@@ -165,6 +149,12 @@ import static java.time.Instant.now;
 						try ( final OutputStream output=Files.newOutputStream(file) ) {
 
 							return encoder.apply(output);
+
+						} catch ( final Exception e ) {
+							
+							Files.delete(file);
+
+							throw e;
 
 						} finally {
 
