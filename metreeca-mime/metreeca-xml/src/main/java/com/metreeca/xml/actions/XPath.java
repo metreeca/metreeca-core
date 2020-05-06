21e911a15ffe53ad9f27b06caeeca5c7beb91040
Alessandro Bollini <22@metreeca.com>
2020-05-06 05:48:00 +0200

    XPath / normalize links

--- metreeca-mime/metreeca-xml/src/main/java/com/metreeca/xml/actions/XPath.java
+++ metreeca-mime/metreeca-xml/src/main/java/com/metreeca/xml/actions/XPath.java
@@ -1,18 +1,5 @@
 /*
- * Copyright © 2013-2020 Metreeca srl. All rights reserved.
- *
- * This file is part of Metreeca/Link.
- *
- * Metreeca/Link is free software: you can redistribute it and/or modify it under the terms
- * of the GNU Affero General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or(at your option) any later version.
- *
- * Metreeca/Link is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License along with Metreeca/Link.
- * If not, see <http://www.gnu.org/licenses/>.
+ * Copyright © 2020 Metreeca srl. All rights reserved.
  */
 
 package com.metreeca.xml.actions;
@@ -307,7 +294,7 @@ public final class XPath {
         return node(xpath).map(Node::getTextContent).map(s -> {
             try {
 
-                return base == null ? s : base.resolve(s).toString();
+                return base == null ? s : base.resolve(s).normalize().toString();
 
             } catch ( final IllegalArgumentException e ) {
 
@@ -337,7 +324,7 @@ public final class XPath {
         return nodes(xpath).map(Node::getTextContent).map(s -> {
             try {
 
-                return base == null ? s : base.resolve(s).toString();
+                return base == null ? s : base.resolve(s).normalize().toString();
 
             } catch ( final IllegalArgumentException e ) {
 
