9c7869a4a7603b1a7b22e7171eb57e11b6d179b6
Alessandro Bollini <22@metreeca.com>
2020-09-15 22:13:11 +0200

    Range / Fix rendering in toString() method

--- metreeca-json/src/main/java/com/metreeca/json/shapes/Range.java
+++ metreeca-json/src/main/java/com/metreeca/json/shapes/Range.java
@@ -93,7 +93,7 @@ public final class Range extends Shape {
 	}
 
 	@Override public String toString() {
-		return "in("+(values.isEmpty() ? "" : values.stream()
+		return "range("+(values.isEmpty() ? "" : values.stream()
 				.map(v -> v.toString().replace("\n", "\n\t"))
 				.collect(joining(",\n\t", "\n\t", "\n"))
 		)+")";
