4be9823be21bc4072f104af5bac5a0bdb1da854a
Alessandro Bollini <22@metreeca.com>
2020-10-15 18:10:59 +0200

    GraphProcessor / Support tagged literal

--- metreeca-back/metreeca-rdf4j/src/main/java/com/metreeca/rdf4j/assets/GraphProcessor.java
+++ metreeca-back/metreeca-rdf4j/src/main/java/com/metreeca/rdf4j/assets/GraphProcessor.java
@@ -755,6 +755,10 @@ abstract class GraphProcessor {
 				throw new UnsupportedOperationException("focus range constraint");
 			}
 
+			@Override public Snippet probe(final Lang lang) {
+				throw new UnsupportedOperationException("focus lang constraint");
+			}
+
 
 			@Override public Snippet probe(final MinExclusive minExclusive) {
 				return snippet("filter ( {source} > {value} )", var(source), format(value(minExclusive.limit())));
@@ -819,6 +823,10 @@ abstract class GraphProcessor {
 
 				return any.values().size() > 1 ? values(source, values(any.values())) : nothing();
 
+			}
+
+			@Override public Snippet probe(final Localized localized) {
+				throw new UnsupportedOperationException("focus localized constraint");
 			}
 
 
